57e84adb18392794748bfb66c3e89d42ccb28633
Maciej Mikosik <maciej@mikosik.com>
2020-04-03 17:50:04 +0200

    used visitor pattern to handle sentences

--- src/com/mikosik/lang/tool/Compiler.java
+++ src/com/mikosik/lang/tool/Compiler.java
@@ -2,75 +2,71 @@ package com.mikosik.lang.tool;
 
 import static com.mikosik.lang.common.Check.check;
 import static com.mikosik.lang.common.Collections.first;
-import static com.mikosik.lang.common.Collections.skipFirst;
 import static com.mikosik.lang.model.def.Definition.definition;
 import static com.mikosik.lang.model.runtime.Application.application;
 import static com.mikosik.lang.model.runtime.Lambda.lambda;
 import static com.mikosik.lang.model.runtime.Variable.variable;
-import static com.mikosik.lang.model.syntax.BracketType.CURLY;
 import static com.mikosik.lang.model.syntax.BracketType.ROUND;
-import static com.mikosik.lang.model.syntax.Sentence.sentence;
+import static com.mikosik.lang.model.syntax.Visitor.visit;
 
 import com.mikosik.lang.model.def.Definition;
 import com.mikosik.lang.model.runtime.Expression;
 import com.mikosik.lang.model.syntax.Bracket;
 import com.mikosik.lang.model.syntax.Sentence;
 import com.mikosik.lang.model.syntax.Syntax;
+import com.mikosik.lang.model.syntax.Visitor;
 import com.mikosik.lang.model.syntax.Word;
 
 public class Compiler {
   public static Definition compileDefinition(Sentence sentence) {
-    return definition(
-        ((Word) first(sentence.parts)).string,
-        compileLambda(sentence(skipFirst(sentence.parts))));
+    return visit(sentence, new Visitor<Definition>() {
+      protected Definition visit(Word head, Sentence tail) {
+        return definition(head.string, compileLambda(tail));
+      }
+    });
   }
 
   public static Expression compileExpression(Sentence sentence) {
-    if (first(sentence.parts) instanceof Word) {
-      return compileApplication(sentence);
-    } else if (first(sentence.parts) instanceof Bracket) {
-      return compileLambda(sentence);
-    } else {
-      throw new RuntimeException();
-    }
+    return visit(sentence, new Visitor<Expression>() {
+      protected Expression visit(Word head, Sentence tail) {
+        return compileApplication(sentence);
+      }
+
+      protected Expression visitRound(Bracket head, Sentence tail) {
+        return compileLambda(sentence);
+      }
+    });
   }
 
   public static Expression compileApplication(Sentence sentence) {
-    return compileApplication(
-        (Word) first(sentence.parts),
-        sentence(skipFirst(sentence.parts)));
-  }
-
-  private static Expression compileApplication(Word head, Sentence tail) {
-    Expression expression = variable(head.string);
-    for (Syntax part : tail.parts) {
-      Bracket bracket = (Bracket) part;
-      check(bracket.type == ROUND);
-      expression = application(
-          expression,
-          compileApplication(bracket.sentence));
-    }
-    return expression;
+    return visit(sentence, new Visitor<Expression>() {
+      protected Expression visit(Word head, Sentence tail) {
+        Expression expression = variable(head.string);
+        for (Syntax part : tail.parts) {
+          Bracket bracket = (Bracket) part;
+          check(bracket.type == ROUND);
+          expression = application(
+              expression,
+              compileApplication(bracket.sentence));
+        }
+        return expression;
+      }
+    });
   }
 
   public static Expression compileLambda(Sentence sentence) {
-    return compileLambda(
-        (Bracket) first(sentence.parts),
-        sentence(skipFirst(sentence.parts)));
-  }
+    return visit(sentence, new Visitor<Expression>() {
+      protected Expression visitRound(Bracket head, Sentence tail) {
+        return lambda(
+            parameterIn(head),
+            compileLambda(tail));
+      }
 
-  public static Expression compileLambda(Bracket head, Sentence tail) {
-    Bracket bracket = head;
-    if (bracket.type == ROUND) {
-      return lambda(
-          parameterIn(bracket),
-          compileLambda(tail));
-    } else if (bracket.type == CURLY) {
-      // TODO rest of sentence after bracket is ignored right now
-      return compileExpression(bracket.sentence);
-    } else {
-      throw new RuntimeException();
-    }
+      protected Expression visitCurly(Bracket head, Sentence tail) {
+        // TODO rest of sentence after bracket is ignored right now
+        return compileExpression(head.sentence);
+      }
+    });
   }
 
   private static String parameterIn(Bracket bracket) {
