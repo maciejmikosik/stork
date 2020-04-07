049cc2e24dee955be46e0e0cde378e57a9155bc3
Maciej Mikosik <maciej@mikosik.com>
2020-04-07 06:26:31 +0200

    created first simple function tests

--- test/java/com/mikosik/lang/RunAllTests.java
+++ test/java/com/mikosik/lang/RunAllTests.java
@@ -1,7 +1,20 @@
 package com.mikosik.lang;
 
+import static com.mikosik.lang.TestSimpleFunctions.testSimpleFunctions;
+import static org.quackery.report.Reports.count;
+import static org.quackery.report.Reports.format;
+import static org.quackery.run.Runners.run;
+
+import org.quackery.Test;
+
 public class RunAllTests {
   public static void main(String[] args) {
+    Test report = run(testSimpleFunctions());
+    System.out.println(format(report));
 
+    int failures = count(Throwable.class, report);
+    if (failures > 0) {
+      throw new AssertionError();
+    }
   }
 }
