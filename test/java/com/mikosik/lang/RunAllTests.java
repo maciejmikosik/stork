c3b2be072d9641bcd5c81de5e52271e7f36e9dcb
Maciej Mikosik <maciej@mikosik.com>
2020-04-07 10:21:47 +0200

    reported details of failed tests

--- test/java/com/mikosik/lang/RunAllTests.java
+++ test/java/com/mikosik/lang/RunAllTests.java
@@ -1,7 +1,7 @@
 package com.mikosik.lang;
 
+import static com.mikosik.lang.MoreReports.formatExceptions;
 import static com.mikosik.lang.TestSimpleFunctions.testSimpleFunctions;
-import static org.quackery.report.Reports.count;
 import static org.quackery.report.Reports.format;
 import static org.quackery.run.Runners.run;
 
@@ -10,11 +10,8 @@ import org.quackery.Test;
 public class RunAllTests {
   public static void main(String[] args) {
     Test report = run(testSimpleFunctions());
-    System.out.println(format(report));
 
-    int failures = count(Throwable.class, report);
-    if (failures > 0) {
-      throw new AssertionError();
-    }
+    System.out.println(formatExceptions(report));
+    System.out.println(format(report));
   }
 }
