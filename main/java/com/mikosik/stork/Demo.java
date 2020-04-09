5c3b16b888fa32020b013e1d71caa4af0b9a06b2
Maciej Mikosik <maciej@mikosik.com>
2020-04-09 09:11:45 +0200

    moved building list of definitions outside library

--- main/java/com/mikosik/stork/Demo.java
+++ main/java/com/mikosik/stork/Demo.java
@@ -10,11 +10,13 @@ import static com.mikosik.stork.tool.Printer.printer;
 import static com.mikosik.stork.tool.Runner.runner;
 import static com.mikosik.stork.tool.Runtime.runtime;
 import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
+import java.util.List;
 
 import com.mikosik.stork.model.def.Definition;
 import com.mikosik.stork.model.def.Library;
@@ -24,18 +26,18 @@ import com.mikosik.stork.tool.Printer;
 import com.mikosik.stork.tool.Runner;
 
 public class Demo {
-  private static Library library = library();
 
   public static void main(String[] args) {
-    stream(readFile("core.stork").split("\n\n"))
+    List<Definition> definitions = stream(readFile("core.stork").split("\n\n"))
         .map(String::trim)
         .filter(source -> source.length() > 0)
         .map(Demo::definition)
-        .forEach(library::add);
+        .collect(toList());
 
+    Library library = library(definitions);
     installCore(library);
 
-    show("main");
+    show("main", library);
   }
 
   private static Definition definition(String source) {
@@ -43,7 +45,7 @@ public class Demo {
     return compileDefinition(sentence);
   }
 
-  private static void show(String source) {
+  private static void show(String source, Library library) {
     Sentence sentence = parse(stream(source));
     Expression expression = compileExpression(sentence);
 
--- main/java/com/mikosik/stork/model/def/Library.java
+++ main/java/com/mikosik/stork/model/def/Library.java
@@ -2,28 +2,24 @@ package com.mikosik.stork.model.def;
 
 import static com.mikosik.stork.model.def.Definition.definition;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import com.mikosik.stork.model.runtime.Expression;
 
 public class Library {
   // TODO make immutable
-  public final List<Definition> definitions = new LinkedList<>();
+  public final List<Definition> definitions;
 
-  private Library() {}
+  private Library(List<Definition> definitions) {
+    this.definitions = definitions;
+  }
 
-  public static Library library() {
-    return new Library();
+  public static Library library(List<Definition> definitions) {
+    return new Library(definitions);
   }
 
   public Library define(String name, Expression expression) {
     definitions.add(definition(name, expression));
-    return this;
-  }
-
-  public Library add(Definition definition) {
-    definitions.add(definition);
     return this;
   }
 }
