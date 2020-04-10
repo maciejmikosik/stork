5ad6115f5060f337917f0339c15a73a2d61fdf9e
Maciej Mikosik <maciej@mikosik.com>
2020-04-10 18:05:46 +0200

    tested function library

--- test/java/com/mikosik/stork/RunAllTests.java
+++ test/java/com/mikosik/stork/RunAllTests.java
@@ -2,6 +2,7 @@ package com.mikosik.stork;
 
 import static com.mikosik.stork.MoreReports.formatExceptions;
 import static com.mikosik.stork.TestBooleanLibrary.testBooleanLibrary;
+import static com.mikosik.stork.TestFunctionLibrary.testFunctionLibrary;
 import static com.mikosik.stork.TestOptionalLibrary.testOptionalLibrary;
 import static com.mikosik.stork.TestSimpleFunctions.testSimpleFunctions;
 import static org.quackery.Suite.suite;
@@ -15,6 +16,7 @@ public class RunAllTests {
   public static void main(String[] args) {
     Suite test = suite("test basics")
         .add(testSimpleFunctions())
+        .add(testFunctionLibrary())
         .add(testBooleanLibrary())
         .add(testOptionalLibrary());
     Test report = run(test);
---
+++ test/java/com/mikosik/stork/TestFunctionLibrary.java
@@ -0,0 +1,32 @@
+package com.mikosik.stork;
+
+import static com.mikosik.stork.StorkTest.storkTest;
+import static org.quackery.Suite.suite;
+
+import org.quackery.Suite;
+import org.quackery.Test;
+
+public class TestFunctionLibrary {
+  public static Suite testFunctionLibrary() {
+    return suite("test function library")
+        .add(suite("self")
+            .add(testEqual("self(x)", "x")))
+        .add(suite("compose")
+            .add(testEqual("compose(f)(g)(x)", "f(g(x))"))
+            .add(testEqual("compose(f)(compose(g)(h))(x)", "f(g(h(x)))"))
+            .add(testEqual("compose(compose(f)(g))(h)(x)", "f(g(h(x)))"))
+            .add(testEqual("compose(compose(f)(g))(compose(h)(i))(x)", "f(g(h(i(x))))")))
+        .add(suite("flip")
+            .add(testEqual("flip(f)(x)(y)", "f(y)(x)"))
+            .add(testEqual("flip(f)(x)(y)(z)", "f(y)(x)(z)")));
+  }
+
+  private static Test testEqual(String expression, String expected) {
+    return storkTest()
+        .givenImported("core.stork")
+        .givenImported("function.stork")
+        .givenMocks("x", "y", "z", "f", "g", "h", "i")
+        .when(expression)
+        .thenReturned(expected);
+  }
+}
