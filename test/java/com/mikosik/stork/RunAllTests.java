418eeef6cb9c3709bf0ad48d06c5df8800d6a91b
Maciej Mikosik <maciej@mikosik.com>
2020-04-10 16:58:24 +0200

    tested optional library

--- test/java/com/mikosik/stork/RunAllTests.java
+++ test/java/com/mikosik/stork/RunAllTests.java
@@ -2,18 +2,22 @@ package com.mikosik.stork;
 
 import static com.mikosik.stork.MoreReports.formatExceptions;
 import static com.mikosik.stork.TestBooleanLibrary.testBooleanLibrary;
+import static com.mikosik.stork.TestOptionalLibrary.testOptionalLibrary;
 import static com.mikosik.stork.TestSimpleFunctions.testSimpleFunctions;
 import static org.quackery.Suite.suite;
 import static org.quackery.report.Reports.format;
 import static org.quackery.run.Runners.run;
 
+import org.quackery.Suite;
 import org.quackery.Test;
 
 public class RunAllTests {
   public static void main(String[] args) {
-    Test report = run(suite("test basics")
+    Suite test = suite("test basics")
         .add(testSimpleFunctions())
-        .add(testBooleanLibrary()));
+        .add(testBooleanLibrary())
+        .add(testOptionalLibrary());
+    Test report = run(test);
 
     System.out.println(formatExceptions(report));
     System.out.println(format(report));
