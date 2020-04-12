a96317c1004e514062ec4e74e71f194d20c84d93
Maciej Mikosik <maciej@mikosik.com>
2020-04-12 21:37:39 +0200

    replaced sentence visitor by switcher

--- main/java/com/mikosik/stork/tool/Modeler.java
+++ main/java/com/mikosik/stork/tool/Modeler.java
@@ -9,6 +9,7 @@ import static com.mikosik.stork.data.model.Parameter.parameter;
 import static com.mikosik.stork.data.model.Primitive.primitive;
 import static com.mikosik.stork.data.model.Variable.variable;
 import static com.mikosik.stork.data.syntax.BracketType.ROUND;
+import static com.mikosik.stork.data.syntax.SentenceSwitcher.sentenceSwitcherReturning;
 
 import java.math.BigInteger;
 
@@ -17,72 +18,55 @@ import com.mikosik.stork.data.model.Expression;
 import com.mikosik.stork.data.model.Parameter;
 import com.mikosik.stork.data.syntax.Bracket;
 import com.mikosik.stork.data.syntax.Sentence;
-import com.mikosik.stork.data.syntax.SentenceVisitor;
 import com.mikosik.stork.data.syntax.Syntax;
 import com.mikosik.stork.data.syntax.Word;
 
 public class Modeler {
   public static Definition modelDefinition(Sentence sentence) {
-    SentenceVisitor<Definition> visitor = new SentenceVisitor<Definition>() {
-      protected Definition visitLabel(Word head, Sentence tail) {
-        return definition(head.string, modelLambda(tail));
-      }
-    };
-    return visitor.visit(sentence);
+    return sentenceSwitcherReturning(Definition.class)
+        .ifLabel((word, tail) -> definition(word.string, modelLambda(tail)))
+        .apply(sentence);
   }
 
   public static Expression modelExpression(Sentence sentence) {
-    SentenceVisitor<Expression> visitor = new SentenceVisitor<Expression>() {
-      protected Expression visitLabel(Word head, Sentence tail) {
-        return modelApplication(sentence);
-      }
-
-      protected Expression visitInteger(Word head, Sentence tail) {
-        if (tail.parts.available()) {
-          throw new RuntimeException("integer cannot be followed by sentence");
-        } else {
-          return primitive(new BigInteger(head.string));
-        }
-      }
-
-      protected Expression visitRound(Bracket head, Sentence tail) {
-        return modelLambda(sentence);
-      }
-    };
-    return visitor.visit(sentence);
+    return sentenceSwitcherReturning(Expression.class)
+        .ifLabel((word, tail) -> modelApplication(sentence))
+        .ifInteger((word, tail) -> {
+          if (tail.parts.available()) {
+            throw new RuntimeException("integer cannot be followed by sentence");
+          } else {
+            return primitive(new BigInteger(word.string));
+          }
+        })
+        .ifRoundBracket((bracket, tail) -> modelLambda(sentence))
+        .apply(sentence);
   }
 
   private static Expression modelApplication(Sentence sentence) {
-    SentenceVisitor<Expression> visitor = new SentenceVisitor<Expression>() {
-      protected Expression visitLabel(Word head, Sentence tail) {
-        Expression expression = variable(head.string);
-        for (Syntax part : tail.parts) {
-          Bracket bracket = (Bracket) part;
-          check(bracket.type == ROUND);
-          expression = application(
-              expression,
-              modelExpression(bracket.sentence));
-        }
-        return expression;
-      }
-    };
-    return visitor.visit(sentence);
+    return sentenceSwitcherReturning(Expression.class)
+        .ifLabel((word, tail) -> {
+          Expression expression = variable(word.string);
+          for (Syntax part : tail.parts) {
+            Bracket bracket = (Bracket) part;
+            check(bracket.type == ROUND);
+            expression = application(
+                expression,
+                modelExpression(bracket.sentence));
+          }
+          return expression;
+        }).apply(sentence);
   }
 
   private static Expression modelLambda(Sentence sentence) {
-    SentenceVisitor<Expression> visitor = new SentenceVisitor<Expression>() {
-      protected Expression visitRound(Bracket head, Sentence tail) {
-        Parameter parameter = parameterIn(head);
-        Expression body = bind(parameter, modelLambda(tail));
-        return lambda(parameter, body);
-      }
-
-      protected Expression visitCurly(Bracket head, Sentence tail) {
+    return sentenceSwitcherReturning(Expression.class)
+        .ifRoundBracket((bracket, tail) -> {
+          Parameter parameter = parameterIn(bracket);
+          Expression body = bind(parameter, modelLambda(tail));
+          return lambda(parameter, body);
+        })
         // TODO rest of sentence after bracket is ignored right now
-        return modelExpression(head.sentence);
-      }
-    };
-    return visitor.visit(sentence);
+        .ifCurlyBracket((bracket, tail) -> modelExpression(bracket.sentence))
+        .apply(sentence);
   }
 
   private static Parameter parameterIn(Bracket bracket) {
