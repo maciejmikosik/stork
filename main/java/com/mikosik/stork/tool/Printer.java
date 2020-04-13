59ce3676c298c1933ce3ae4f7c7430007b5012e9
Maciej Mikosik <maciej@mikosik.com>
2020-04-13 20:59:42 +0200

    corrected implementation of expression printer

--- main/java/com/mikosik/stork/tool/Printer.java
+++ main/java/com/mikosik/stork/tool/Printer.java
@@ -7,22 +7,18 @@ import com.mikosik.stork.data.model.Expression;
 import com.mikosik.stork.data.model.Lambda;
 
 public class Printer {
-  private Printer() {}
-
-  public static Printer printer() {
-    return new Printer();
-  }
-
-  public String print(Expression expression) {
+  public static String print(Expression expression) {
     return expressionSwitcherReturning(String.class)
         .ifPrimitive(primitive -> primitive.object.toString())
         .ifVariable(variable -> variable.name)
+        .ifParameter(parameter -> parameter.name)
         .ifApplication(application -> format("%s(%s)",
             print(application.function),
             print(application.argument)))
         .ifLambda(lambda -> lambda.body instanceof Lambda
-            ? format("(%s)%s", lambda.parameter, print(lambda.body))
-            : format("(%s){ %s }", lambda.parameter, print(lambda.body)))
+            ? format("(%s)%s", print(lambda.parameter), print(lambda.body))
+            : format("(%s){%s}", print(lambda.parameter), print(lambda.body)))
+        .ifCore(core -> core.toString())
         .apply(expression);
   }
 }
