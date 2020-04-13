e92d319731c5a3e830c82ac2cb6a64dfc9abff00
Maciej Mikosik <maciej@mikosik.com>
2020-04-13 17:00:38 +0200

    improved report of tests failing

--- test/java/com/mikosik/stork/RunAllTests.java
+++ test/java/com/mikosik/stork/RunAllTests.java
@@ -2,8 +2,10 @@ package com.mikosik.stork;
 
 import static com.mikosik.stork.TestRunner.testRunner;
 import static com.mikosik.stork.TestStorkLibraries.testStorkLibraries;
+import static com.mikosik.stork.testing.MoreReports.filter;
 import static com.mikosik.stork.testing.MoreReports.formatExceptions;
 import static org.quackery.Suite.suite;
+import static org.quackery.report.Reports.count;
 import static org.quackery.report.Reports.format;
 import static org.quackery.run.Runners.run;
 
@@ -16,7 +18,18 @@ public class RunAllTests {
         .add(testStorkLibraries());
     Test report = run(test);
 
-    System.out.println(formatExceptions(report));
-    System.out.println(format(report));
+    int total = count(Throwable.class, report);
+    if (total == 0) {
+      System.out.println(format(report));
+      System.out.println("");
+      System.out.println("no failures");
+      System.exit(0);
+    } else {
+      Test failed = filter(Throwable.class, report).get();
+      System.err.println(format(failed));
+      System.err.println("");
+      System.err.println(formatExceptions(failed));
+      System.exit(1);
+    }
   }
 }
