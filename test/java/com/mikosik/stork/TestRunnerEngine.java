8179d5cac9f07d31b8119bb95897ea8cbf6f995c
Maciej Mikosik <maciej@mikosik.com>
2020-04-13 13:51:32 +0200

    tested using integer and lambda as application argument

--- test/java/com/mikosik/stork/TestRunnerEngine.java
+++ test/java/com/mikosik/stork/TestRunnerEngine.java
@@ -8,6 +8,15 @@ import org.quackery.Suite;
 public class TestRunnerEngine {
   public static Suite testRunnerEngine() {
     return suite("test runner engine")
+        .add(storkTest("application argument can be integer")
+            .givenMocks("f")
+            .when("f(2)")
+            .thenReturned("f(2)"))
+        .add(storkTest("application argument can be lambda")
+            .givenMocks("mock")
+            .given("f(g)(x) { g(x) }")
+            .when("f((x){x})(mock)")
+            .thenReturned("mock"))
         .add(storkTest("applied argument is not accidently bound to lambda parameter")
             .givenMocks("f", "x", "y", "z")
             .given("tuple(x)(y)(f) { f(x)(y) }")
