83f45438456c475c0c4fde6e8859f5b5ac200341
Maciej Mikosik <maciej@mikosik.com>
2020-04-20 04:08:39 +0200

    extracted parameter substitution from runner code

--- main/java/com/mikosik/stork/tool/run/RecursiveRunner.java
+++ main/java/com/mikosik/stork/tool/run/RecursiveRunner.java
@@ -1,12 +1,10 @@
 package com.mikosik.stork.tool.run;
 
-import static com.mikosik.stork.data.model.Application.application;
 import static com.mikosik.stork.data.model.ExpressionSwitcher.expressionSwitcherReturning;
-import static com.mikosik.stork.data.model.Lambda.lambda;
+import static com.mikosik.stork.tool.run.Substitute.substitute;
 
 import com.mikosik.stork.data.model.Application;
 import com.mikosik.stork.data.model.Expression;
-import com.mikosik.stork.data.model.Parameter;
 import com.mikosik.stork.tool.Binary;
 
 public class RecursiveRunner implements Runner {
@@ -33,29 +31,11 @@ public class RecursiveRunner implements Runner {
   private Expression run(Application application) {
     Expression function = run(application.function);
     return expressionSwitcherReturning(Expression.class)
-        .ifLambda(lambda -> run(bind(
+        .ifLambda(lambda -> run(substitute(
             lambda.body,
             lambda.parameter,
             application.argument)))
         .ifCore(core -> run(core.apply(run(application.argument))))
         .apply(function);
-  }
-
-  private Expression bind(Expression expression, Parameter parameter, Expression argument) {
-    return expressionSwitcherReturning(Expression.class)
-        .ifApplication(application -> application(
-            bind(application.function, parameter, argument),
-            bind(application.argument, parameter, argument)))
-        .ifVariable(variable -> variable)
-        .ifLambda(lambda -> lambda.parameter == parameter
-            ? expression // TODO test shadowing
-            : lambda(
-                lambda.parameter,
-                bind(lambda.body, parameter, argument)))
-        .ifParameter(foundParameter -> foundParameter == parameter
-            ? argument
-            : foundParameter)
-        .ifPrimitive(primitive -> primitive)
-        .apply(expression);
   }
 }
---
+++ main/java/com/mikosik/stork/tool/run/Substitute.java
@@ -0,0 +1,46 @@
+package com.mikosik.stork.tool.run;
+
+import static com.mikosik.stork.data.model.Application.application;
+import static com.mikosik.stork.data.model.ExpressionSwitcher.expressionSwitcherReturning;
+import static com.mikosik.stork.data.model.Lambda.lambda;
+
+import com.mikosik.stork.data.model.Application;
+import com.mikosik.stork.data.model.Expression;
+import com.mikosik.stork.data.model.Lambda;
+import com.mikosik.stork.data.model.Parameter;
+
+public class Substitute {
+  /**
+   * When running {@link Application} that applies {@link Lambda} on given argument
+   * {@link Expression} it is required to replace each occurrence of lambda's parameter in lambda's
+   * body by given argument. This method performs this.
+   *
+   * @param expression
+   *          expression to perform substitution on (e.g. lambda's body)
+   * @param parameter
+   *          parameter in lambda's body to be substituted by argument
+   * @param argument
+   *          expression to replace parameter
+   * @return expression after performing substitution
+   */
+  public static Expression substitute(
+      Expression expression,
+      Parameter parameter,
+      Expression argument) {
+    return expressionSwitcherReturning(Expression.class)
+        .ifApplication(application -> application(
+            substitute(application.function, parameter, argument),
+            substitute(application.argument, parameter, argument)))
+        .ifVariable(variable -> variable)
+        .ifLambda(lambda -> lambda.parameter == parameter
+            ? expression // TODO test shadowing
+            : lambda(
+                lambda.parameter,
+                substitute(lambda.body, parameter, argument)))
+        .ifParameter(foundParameter -> foundParameter == parameter
+            ? argument
+            : foundParameter)
+        .ifPrimitive(primitive -> primitive)
+        .apply(expression);
+  }
+}
