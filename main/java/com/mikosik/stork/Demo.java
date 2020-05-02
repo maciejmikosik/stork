6a4a9a652a76cf1ddd05875209abfe3a47631dcb
Maciej Mikosik <maciej@mikosik.com>
2020-05-02 23:28:20 +0200

    turned linker into interface

--- main/java/com/mikosik/stork/Demo.java
+++ main/java/com/mikosik/stork/Demo.java
@@ -4,7 +4,7 @@ import static com.mikosik.stork.common.Chains.chainOf;
 import static com.mikosik.stork.lib.Modules.module;
 import static com.mikosik.stork.tool.Default.compileExpression;
 import static com.mikosik.stork.tool.Default.compileModule;
-import static com.mikosik.stork.tool.Linker.link;
+import static com.mikosik.stork.tool.DefaultLinker.defaultLinker;
 import static com.mikosik.stork.tool.run.ExhaustedRunner.exhausted;
 import static com.mikosik.stork.tool.run.LoggingRunner.logging;
 import static com.mikosik.stork.tool.run.ModuleRunner.runner;
@@ -13,6 +13,7 @@ import static com.mikosik.stork.tool.run.Stepper.stepper;
 import com.mikosik.stork.common.Chain;
 import com.mikosik.stork.data.model.Expression;
 import com.mikosik.stork.data.model.Module;
+import com.mikosik.stork.tool.Linker;
 import com.mikosik.stork.tool.run.Runner;
 
 public class Demo {
@@ -20,7 +21,8 @@ public class Demo {
     Chain<Module> modules = chainOf(
         module("integer.stork"),
         compileModule("main { add(add(1)(2))(add(5)(10)) }"));
-    Runner moduleRunner = runner(link(modules));
+    Linker linker = defaultLinker();
+    Runner moduleRunner = runner(linker.link(modules));
     Runner runner = exhausted(logging(stepper(moduleRunner)));
     Expression main = compileExpression("main");
     runner.run(main);
--- test/java/com/mikosik/stork/testing/StorkTest.java
+++ test/java/com/mikosik/stork/testing/StorkTest.java
@@ -9,7 +9,7 @@ import static com.mikosik.stork.data.model.Definition.definition;
 import static com.mikosik.stork.data.model.Module.module;
 import static com.mikosik.stork.testing.Mock.mock;
 import static com.mikosik.stork.tool.Default.compileExpression;
-import static com.mikosik.stork.tool.Linker.link;
+import static com.mikosik.stork.tool.DefaultLinker.defaultLinker;
 import static com.mikosik.stork.tool.Printer.print;
 import static com.mikosik.stork.tool.run.ExhaustedRunner.exhausted;
 import static com.mikosik.stork.tool.run.ModuleRunner.runner;
@@ -25,6 +25,7 @@ import com.mikosik.stork.data.model.Expression;
 import com.mikosik.stork.data.model.Module;
 import com.mikosik.stork.lib.Modules;
 import com.mikosik.stork.tool.Default;
+import com.mikosik.stork.tool.Linker;
 import com.mikosik.stork.tool.run.Runner;
 
 public class StorkTest extends Case {
@@ -141,7 +142,8 @@ public class StorkTest extends Case {
             module(definitions),
             module(mocks)),
         modules);
-    Runner runner = exhausted(stepper(runner(link(allModules))));
+    Linker linker = defaultLinker();
+    Runner runner = exhausted(stepper(runner(linker.link(allModules))));
 
     Expression actual = runner.run(compileExpression(whenExpression));
     Expression expected = runner.run(compileExpression(thenReturnedExpression));
