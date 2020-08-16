65e4816fbc3a9ac9b8be6129d836379fad5df775
Maciej Mikosik <maciej@mikosik.com>
2020-08-16 19:22:10 +0200

    extracted function testing if expression is computable

--- main/java/com/mikosik/stork/tool/comp/VerbComputer.java
+++ main/java/com/mikosik/stork/tool/comp/VerbComputer.java
@@ -2,10 +2,8 @@ package com.mikosik.stork.tool.comp;
 
 import static com.mikosik.stork.data.model.comp.Computation.computation;
 import static com.mikosik.stork.data.model.comp.Function.function;
+import static com.mikosik.stork.tool.common.Computations.isComputable;
 
-import com.mikosik.stork.data.model.Application;
-import com.mikosik.stork.data.model.Expression;
-import com.mikosik.stork.data.model.Variable;
 import com.mikosik.stork.data.model.Verb;
 import com.mikosik.stork.data.model.comp.Argument;
 import com.mikosik.stork.data.model.comp.Computation;
@@ -31,17 +29,12 @@ public class VerbComputer implements Computer {
   }
 
   private Computation compute(Verb verb, Argument argument) {
-    return requiresComputing(argument.expression)
+    return isComputable(argument.expression)
         ? computation(
             argument.expression,
             function(verb, argument.stack))
         : computation(
             verb.apply(argument.expression),
             argument.stack);
-  }
-
-  private static boolean requiresComputing(Expression expression) {
-    return expression instanceof Variable
-        || expression instanceof Application;
   }
 }
