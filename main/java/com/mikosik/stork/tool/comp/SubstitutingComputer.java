87210424092081e8fa8ba551d582df68a9c4a03c
Maciej Mikosik <maciej@mikosik.com>
2020-08-16 19:47:58 +0200

    inlined common for lambda substitution

--- main/java/com/mikosik/stork/tool/comp/SubstitutingComputer.java
+++ main/java/com/mikosik/stork/tool/comp/SubstitutingComputer.java
@@ -1,9 +1,13 @@
 package com.mikosik.stork.tool.comp;
 
+import static com.mikosik.stork.data.model.Application.application;
+import static com.mikosik.stork.data.model.Lambda.lambda;
 import static com.mikosik.stork.data.model.comp.Computation.computation;
-import static com.mikosik.stork.tool.common.Substitute.substitute;
 
+import com.mikosik.stork.data.model.Application;
+import com.mikosik.stork.data.model.Expression;
 import com.mikosik.stork.data.model.Lambda;
+import com.mikosik.stork.data.model.Parameter;
 import com.mikosik.stork.data.model.comp.Argument;
 import com.mikosik.stork.data.model.comp.Computation;
 
@@ -21,15 +25,52 @@ public class SubstitutingComputer implements Computer {
   public Computation compute(Computation computation) {
     return computation.expression instanceof Lambda
         && computation.stack instanceof Argument
-            ? compute(
+            ? substitute(
                 (Lambda) computation.expression,
                 (Argument) computation.stack)
             : nextComputer.compute(computation);
   }
 
-  private Computation compute(Lambda lambda, Argument argument) {
+  private Computation substitute(Lambda lambda, Argument argument) {
     return computation(
-        substitute(lambda, argument.expression),
+        substitute(lambda.body, lambda.parameter, argument.expression),
         argument.stack);
+  }
+
+  private static Expression substitute(
+      Expression expression,
+      Parameter parameter,
+      Expression argument) {
+    if (expression instanceof Application) {
+      return substitute((Application) expression, parameter, argument);
+    } else if (expression instanceof Lambda) {
+      return substitute((Lambda) expression, parameter, argument);
+    } else if (expression instanceof Parameter) {
+      return expression == parameter
+          ? argument
+          : expression;
+    } else {
+      return expression;
+    }
+  }
+
+  private static Expression substitute(
+      Application application,
+      Parameter parameter,
+      Expression argument) {
+    return application(
+        substitute(application.function, parameter, argument),
+        substitute(application.argument, parameter, argument));
+  }
+
+  private static Expression substitute(
+      Lambda lambda,
+      Parameter parameter,
+      Expression argument) {
+    return lambda.parameter == parameter
+        ? lambda // TODO test shadowing
+        : lambda(
+            lambda.parameter,
+            substitute(lambda.body, parameter, argument));
   }
 }
