7a234fb4752dbb9f525c42b2db17eec4c12467df
Maciej Mikosik <maciej@mikosik.com>
2020-10-01 15:47:27 +0200

    implemented translation from stork string to java string

--- main/java/com/mikosik/stork/tool/common/Translate.java
+++ main/java/com/mikosik/stork/tool/common/Translate.java
@@ -4,8 +4,11 @@ import static com.mikosik.stork.common.Strings.reverse;
 import static com.mikosik.stork.data.model.Application.application;
 import static com.mikosik.stork.data.model.Integer.integer;
 import static com.mikosik.stork.data.model.Variable.variable;
+import static com.mikosik.stork.tool.common.Invocation.asInvocation;
 
+import java.io.ByteArrayOutputStream;
 import java.math.BigInteger;
+import java.util.Iterator;
 
 import com.mikosik.stork.data.model.Expression;
 import com.mikosik.stork.data.model.Integer;
@@ -37,5 +40,26 @@ public class Translate {
   public static BigInteger asJavaBigInteger(Expression expression) {
     Integer integer = (Integer) expression;
     return integer.value;
+  }
+
+  public static String asJavaString(Expression expression) {
+    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    writeTo(buffer, expression);
+    return new String(buffer.toByteArray());
+  }
+
+  private static void writeTo(ByteArrayOutputStream buffer, Expression expression) {
+    Invocation invocation = asInvocation(expression);
+    if (invocation.function.name.equals("some")) {
+      Iterator<Expression> iterator = invocation.arguments.iterator();
+      BigInteger oneByte = asJavaBigInteger(iterator.next());
+      Expression tail = iterator.next();
+      buffer.write(oneByte.intValueExact());
+      writeTo(buffer, tail);
+    } else if (invocation.function.name.equals("none")) {
+      return;
+    } else {
+      throw new RuntimeException();
+    }
   }
 }
