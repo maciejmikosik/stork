56cd3b95fae7b6dad53251ecb115130d521bc241
Maciej Mikosik <maciej@mikosik.com>
2020-10-31 15:51:39 +0100

    used only module compilation in computer tests

--- test/java/com/mikosik/stork/TestEverything.java
+++ test/java/com/mikosik/stork/TestEverything.java
@@ -26,33 +26,34 @@ public class TestEverything {
 
   private static Test testComputer() {
     return suite("compiling and computing")
-        .add(computerTest("returning constant")
-            .given("main{x}")
-            .when("main")
-            .then("x"))
-        .add(computerTest("returning argument")
-            .given("main(argument){argument}")
-            .when("main(x)")
-            .then("x"))
-        .add(computerTest("forwarding argument and result")
-            .given("identity(arg){arg}")
-            .given("main(arg){identity(arg)}")
-            .when("main(x)")
-            .then("x"))
-        .add(computerTest("application argument can be integer")
-            .when("f(2)")
-            .then("f(2)"))
-        .add(computerTest("application argument can be lambda")
-            .given("apply(f)(x) { f(x) }")
-            .when("apply((x){x})(a)")
-            .then("a"))
-        .add(computerTest("integer is ignored when binding")
-            .given("function(x) { x(2) }")
-            .when("function(f)")
-            .then("f(2)"))
-        .add(computerTest("applied argument is not accidently bound to lambda parameter")
-            .given("tuple(x)(y)(f) { f(x)(y) }")
-            .when("tuple(y)(z)(f)")
-            .then("f(y)(z)"));
+        .add(computerTest("returning constant", ""
+            + " constant { value } "
+            + " when { constant }  "
+            + " then { value }     "))
+        .add(computerTest("returning argument", ""
+            + " identity(x) { x }    "
+            + " when { identity(y) } "
+            + " then { y }           "))
+        .add(computerTest("forwarding argument and result", ""
+            + " identity(x) { x }           "
+            + " function(x) { identity(x) } "
+            + " when { function(x) }        "
+            + " then { x }                  "))
+        .add(computerTest("application argument can be integer", ""
+            + " identity(x) { x }    "
+            + " when { identity(2) } "
+            + " then { 2 }           "))
+        .add(computerTest("application argument can be lambda", ""
+            + " apply(f)(x) { f(x) }      "
+            + " when { apply((x){x})(y) } "
+            + " then { y }                "))
+        .add(computerTest("integer is ignored when binding", ""
+            + " apply2(f) { f(2) } "
+            + " when { apply2(g) } "
+            + " then { g(2) }      "))
+        .add(computerTest("applied argument is not accidently bound to lambda parameter", ""
+            + " tuple(x)(y)(f) { f(x)(y) } "
+            + " when { tuple(y)(z)(f) }    "
+            + " then { f(y)(z) }           "));
   }
 }
