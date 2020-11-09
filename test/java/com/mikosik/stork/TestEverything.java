bf4c2d6f0602ebd9618f72d5c1faaef71ddc8dc0
Maciej Mikosik <maciej@mikosik.com>
2020-11-09 10:23:42 +0100

    added newlines to stork code in tests

--- test/java/com/mikosik/stork/TestEverything.java
+++ test/java/com/mikosik/stork/TestEverything.java
@@ -1,5 +1,6 @@
 package com.mikosik.stork;
 
+import static com.mikosik.stork.common.Strings.line;
 import static com.mikosik.stork.testing.ComputerTest.computerTest;
 import static com.mikosik.stork.testing.ModuleTest.testModule;
 import static com.mikosik.stork.testing.ProgramTest.programTest;
@@ -25,63 +26,63 @@ public class TestEverything {
   private static Test testComputer() {
     return suite("compiling and computing")
         .add(computerTest("returning constant", ""
-            + " constant { value } "
-            + " when { constant }  "
-            + " then { value }     "))
+            + line(" constant { value } ")
+            + line(" when { constant }  ")
+            + line(" then { value }     ")))
         .add(computerTest("returning argument", ""
-            + " identity(x) { x }    "
-            + " when { identity(y) } "
-            + " then { y }           "))
+            + line(" identity(x) { x }    ")
+            + line(" when { identity(y) } ")
+            + line(" then { y }           ")))
         .add(computerTest("forwarding argument and result", ""
-            + " identity(x) { x }           "
-            + " function(x) { identity(x) } "
-            + " when { function(x) }        "
-            + " then { x }                  "))
+            + line(" identity(x) { x }           ")
+            + line(" function(x) { identity(x) } ")
+            + line(" when { function(x) }        ")
+            + line(" then { x }                  ")))
         .add(computerTest("application argument can be integer", ""
-            + " identity(x) { x }    "
-            + " when { identity(2) } "
-            + " then { 2 }           "))
+            + line(" identity(x) { x }    ")
+            + line(" when { identity(2) } ")
+            + line(" then { 2 }           ")))
         .add(computerTest("application argument can be lambda", ""
-            + " apply(f)(x) { f(x) }      "
-            + " when { apply((x){x})(y) } "
-            + " then { y }                "))
+            + line(" apply(f)(x) { f(x) }      ")
+            + line(" when { apply((x){x})(y) } ")
+            + line(" then { y }                ")))
         .add(computerTest("integer is ignored when binding", ""
-            + " apply2(f) { f(2) } "
-            + " when { apply2(g) } "
-            + " then { g(2) }      "))
+            + line(" apply2(f) { f(2) } ")
+            + line(" when { apply2(g) } ")
+            + line(" then { g(2) }      ")))
         .add(computerTest("applied argument is not accidently bound to lambda parameter", ""
-            + " tuple(x)(y)(f) { f(x)(y) } "
-            + " when { tuple(y)(z)(f) }    "
-            + " then { f(y)(z) }           "));
+            + line(" tuple(x)(y)(f) { f(x)(y) } ")
+            + line(" when { tuple(y)(z)(f) }    ")
+            + line(" then { f(y)(z) }           ")));
   }
 
   private static Suite testProgram() {
     return suite("program")
         .add(programTest("uses string literal", ""
-            + " main {  "
-            + "   'abc' "
-            + " }       ",
+            + line(" main {  ")
+            + line("   'abc' ")
+            + line(" }       "),
             "abc"))
         .add(programTest("uses core function", ""
-            + " build {                                         "
-            + "   module                                        "
-            + "     (importAs('append')('stork.stream.append')) "
-            + " }                                               "
-            + "                                                 "
-            + " main {                                          "
-            + "   append('!')('Hello World')                    "
-            + " }                                               ",
+            + line(" build {                                         ")
+            + line("   module                                        ")
+            + line("     (importAs('append')('stork.stream.append')) ")
+            + line(" }                                               ")
+            + line("                                                 ")
+            + line(" main {                                          ")
+            + line("   append('!')('Hello World')                    ")
+            + line(" }                                               "),
             "Hello World!"))
         .add(programTest("handles character codes", ""
-            + " build {                                         "
-            + "   module                                        "
-            + "     (importAs('append')('stork.stream.append')) "
-            + "     (importAs('single')('stork.stream.single')) "
-            + " }                                               "
-            + "                                                 "
-            + " main {                                          "
-            + "   append(single(33))('Hello World')             "
-            + " }                                               ",
+            + line(" build {                                         ")
+            + line("   module                                        ")
+            + line("     (importAs('append')('stork.stream.append')) ")
+            + line("     (importAs('single')('stork.stream.single')) ")
+            + line(" }                                               ")
+            + line("                                                 ")
+            + line(" main {                                          ")
+            + line("   append(single(33))('Hello World')             ")
+            + line(" }                                               "),
             "Hello World!"));
   }
 }
