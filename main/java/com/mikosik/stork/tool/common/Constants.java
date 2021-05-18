c5b37b3f66401225b6febba4fdbefdb609f53f2c
Maciej Mikosik <maciej@mikosik.com>
2021-05-18 16:10:44 +0200

    move constants for stream creation

--- main/java/com/mikosik/stork/tool/common/Constants.java
+++ main/java/com/mikosik/stork/tool/common/Constants.java
@@ -7,4 +7,6 @@ import com.mikosik.stork.model.Variable;
 public class Constants {
   public static final Variable TRUE = variable("stork.boolean.true");
   public static final Variable FALSE = variable("stork.boolean.false");
+  public static final Variable SOME = variable("stork.stream.some");
+  public static final Variable NONE = variable("stork.stream.none");
 }
