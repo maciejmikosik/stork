c286bb68a8591c7313dfbb6fb4976c3d238ad989
Maciej Mikosik <maciej@mikosik.com>
2021-06-21 08:28:50 +0200

    made stdin computer reuse caching computer

--- runtime/com/mikosik/stork/program/StdinComputer.java
+++ runtime/com/mikosik/stork/program/StdinComputer.java
@@ -4,20 +4,16 @@ import static com.mikosik.stork.model.Application.application;
 import static com.mikosik.stork.model.Computation.computation;
 import static com.mikosik.stork.model.Integer.integer;
 import static com.mikosik.stork.model.Variable.variable;
-
-import java.util.Map;
-import java.util.WeakHashMap;
+import static com.mikosik.stork.tool.compute.CachingComputer.caching;
 
 import com.mikosik.stork.model.Computation;
 import com.mikosik.stork.model.Expression;
-import com.mikosik.stork.model.Stack;
 import com.mikosik.stork.model.Variable;
 import com.mikosik.stork.tool.compute.Computer;
 
 public class StdinComputer implements Computer {
   private final Variable some = variable("stork.stream.some");
   private final Variable none = variable("stork.stream.none");
-  private final Map<Stdin, Expression> cache = new WeakHashMap<>();
 
   private final Computer computer;
 
@@ -26,19 +22,13 @@ public class StdinComputer implements Computer {
   }
 
   public static Computer stdin(Computer computer) {
-    return new StdinComputer(computer);
+    return caching(new StdinComputer(computer));
   }
 
   public Computation compute(Computation computation) {
     return computation.expression instanceof Stdin
-        ? compute((Stdin) computation.expression, computation.stack)
+        ? computation(compute((Stdin) computation.expression), computation.stack)
         : computer.compute(computation);
-  }
-
-  private Computation compute(Stdin stdin, Stack stack) {
-    return computation(
-        cache.computeIfAbsent(stdin, this::compute),
-        stack);
   }
 
   private Expression compute(Stdin stdin) {
