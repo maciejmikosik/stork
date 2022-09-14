e26d5fe1645d3366a680e4a0a0d865577998453a
Maciej Mikosik <maciej@mikosik.com>
2022-09-14 15:56:40 +0200

    configured debug to inject loggers into fields

--- runtime/com/mikosik/stork/debug/Debug.java
+++ runtime/com/mikosik/stork/debug/Debug.java
@@ -2,6 +2,8 @@ package com.mikosik.stork.debug;
 
 import static com.mikosik.stork.debug.HandlingChainDecorator.handlingChain;
 import static com.mikosik.stork.tool.decompile.Decompiler.decompiler;
+import static org.logbuddy.decorator.ComposedDecorator.compose;
+import static org.logbuddy.decorator.InjectingDecorator.injecting;
 import static org.logbuddy.decorator.InvocationDecorator.invocationDecorator;
 import static org.logbuddy.decorator.TraversingDecorator.traversing;
 import static org.logbuddy.logger.InvocationDepthLogger.invocationDepth;
@@ -17,8 +19,10 @@ import org.logbuddy.Renderer;
 public class Debug {
   public static Decorator configuredDecorator(Path logFile) {
     Renderer<String> renderer = new StorkTextRenderer(decompiler().local());
-    Logger logger = fileLogger(logFile, renderer);
-    Decorator decorator = handlingChain(invocationDecorator(invocationDepth(logger)));
+    Logger logger = invocationDepth(fileLogger(logFile, renderer));
+    Decorator decorator = handlingChain(compose(
+        invocationDecorator(logger),
+        injecting(logger)));
     return traversing(decorator)
         .filter(field -> !Map.class.isAssignableFrom(field.getDeclaringClass()));
   }
