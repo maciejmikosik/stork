a4d86c4669ce75677c293ad8875f7c7bb0c414e3
Maciej Mikosik <maciej@mikosik.com>
2022-11-25 08:49:50 +0100

    refactored reflection usage in decompiler

--- runtime/com/mikosik/stork/common/Reflection.java
+++ runtime/com/mikosik/stork/common/Reflection.java
@@ -7,20 +7,24 @@ import java.util.Optional;
 import java.util.function.Predicate;
 
 public class Reflection {
-  public static Optional<Object> read(Predicate<Field> predicate, Object instance) {
-    return stream(instance.getClass().getDeclaredFields())
-        .filter(predicate::test)
+  public static <T> Optional<T> maybeRead(Predicate<Field> predicate, Object instance) {
+    return (Optional<T>) stream(instance.getClass().getDeclaredFields())
+        .filter(predicate)
         .map(field -> accessible(field))
         .map(field -> read(field, instance))
         .findFirst();
   }
 
-  public static Predicate<Field> signature(Class<?> type, String name) {
+  public static <T> Optional<T> maybeReadOfType(Class<T> type, Object instance) {
+    return maybeRead(isOfType(type), instance);
+  }
+
+  public static Predicate<Field> hasSignature(Class<?> type, String name) {
     return field -> field.getType() == type
         && field.getName().equals(name);
   }
 
-  public static Predicate<Field> type(Class<?> type) {
+  public static Predicate<Field> isOfType(Class<?> type) {
     return field -> field.getType() == type;
   }
 
