0e8eda7fa8d83364b95a97a3e8fe8fe61b201377
Maciej Mikosik <maciej@mikosik.com>
2023-01-11 06:46:27 +0100

    rewritten integers tests

--- runtime/com/mikosik/stork/test/TestCoreLibrary.java
+++ runtime/com/mikosik/stork/test/TestCoreLibrary.java
@@ -8,7 +8,99 @@ import org.quackery.Test;
 public class TestCoreLibrary {
   public static Test testCoreLibrary() {
     return suite("core library")
+        .add(suite("integer")
+            .add(testIntegerCanonical())
+            .add(testIntegerHuge())
+            .add(testIntegerEqual())
+            .add(testIntegerNegate())
+            .add(testIntegerAdd())
+            .add(testIntegerMoreThan())
+            .add(testIntegerEager()))
         .add(testOptional());
+  }
+
+  private static SnippetTest testIntegerCanonical() {
+    return snippetTest("canonical")
+        .test("+0 ", " 0")
+        .test("-0 ", " 0")
+        .test("+00", " 0")
+        .test(" 00", " 0")
+        .test("-00", " 0")
+        .test("+01", " 1")
+        .test(" 01", " 1")
+        .test("-01", "-1");
+  }
+
+  private static Test testIntegerHuge() {
+    return snippetTest("huge")
+        .test("+1000000000000000000000000000000",
+            "  +1000000000000000000000000000000")
+        .test(" 1000000000000000000000000000000",
+            "   1000000000000000000000000000000")
+        .test("-1000000000000000000000000000000",
+            "  -1000000000000000000000000000000");
+  }
+
+  private static Test testIntegerEqual() {
+    return snippetTest("equal")
+        .importing("stork.boolean.true")
+        .importing("stork.boolean.false")
+        .importing("stork.integer.equal")
+        .test("equal(-1)(-1)", "true")
+        .test("equal(-1)( 0)", "false")
+        .test("equal(-1)( 1)", "false")
+        .test("equal( 0)(-1)", "false")
+        .test("equal( 0)( 0)", "true")
+        .test("equal( 0)( 1)", "false")
+        .test("equal( 1)(-1)", "false")
+        .test("equal( 1)( 0)", "false")
+        .test("equal( 1)( 1)", "true");
+  }
+
+  private static Test testIntegerNegate() {
+    return snippetTest("negate")
+        .importing("stork.integer.negate")
+        .test("negate( 5)", "-5")
+        .test("negate(-3)", " 3")
+        .test("negate( 0)", " 0");
+  }
+
+  private static Test testIntegerAdd() {
+    return snippetTest("add")
+        .importing("stork.integer.add")
+        .test("add(  2)( 3)", "  5")
+        .test("add( -1)( 1)", "  0")
+        .test("add(-10)(-5)", "-15");
+  }
+
+  private static Test testIntegerMoreThan() {
+    return snippetTest("moreThan")
+        .importing("stork.boolean.true")
+        .importing("stork.boolean.false")
+        .importing("stork.integer.moreThan")
+        .test("moreThan(-1)(-1)", "false")
+        .test("moreThan(-1)( 0)", "true")
+        .test("moreThan(-1)( 1)", "true")
+        .test("moreThan( 0)(-1)", "false")
+        .test("moreThan( 0)( 0)", "false")
+        .test("moreThan( 0)( 1)", "true")
+        .test("moreThan( 1)(-1)", "false")
+        .test("moreThan( 1)( 0)", "false")
+        .test("moreThan( 1)( 1)", "false");
+  }
+
+  private static Test testIntegerEager() {
+    return snippetTest("eager")
+        .importing("stork.boolean.true")
+        .importing("stork.boolean.false")
+        .importing("stork.integer.equal")
+        .importing("stork.integer.negate")
+        .importing("stork.integer.add")
+        .importing("stork.integer.moreThan")
+        .test("equal(negate(2))(negate(3))", "false")
+        .test("add(add(1)(2))(add(3)(4))", "10")
+        .test("moreThan(negate(3))(negate(4))", "false")
+        .test("negate(negate(3))", "3");
   }
 
   private static Test testOptional() {
