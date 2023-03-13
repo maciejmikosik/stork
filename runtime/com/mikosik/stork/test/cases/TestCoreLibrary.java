d7f4e16a302887d1c9cda24587ff5fde6baf342f
Maciej Mikosik <maciej@mikosik.com>
2023-03-13 12:10:06 +0100

    renamed optional to maybe

--- runtime/com/mikosik/stork/test/cases/TestCoreLibrary.java
+++ runtime/com/mikosik/stork/test/cases/TestCoreLibrary.java
@@ -3,7 +3,7 @@ package com.mikosik.stork.test.cases;
 import static com.mikosik.stork.test.cases.TestBoolean.testBoolean;
 import static com.mikosik.stork.test.cases.TestFunction.testFunction;
 import static com.mikosik.stork.test.cases.TestInteger.testInteger;
-import static com.mikosik.stork.test.cases.TestOptional.testOptional;
+import static com.mikosik.stork.test.cases.TestMaybe.testMaybe;
 import static com.mikosik.stork.test.cases.TestStream.testStream;
 import static org.quackery.Suite.suite;
 
@@ -16,6 +16,6 @@ public class TestCoreLibrary {
         .add(testFunction())
         .add(testInteger())
         .add(testStream())
-        .add(testOptional());
+        .add(testMaybe());
   }
 }
--- runtime/com/mikosik/stork/test/cases/TestMaybe.java
+++ runtime/com/mikosik/stork/test/cases/TestMaybe.java
@@ -5,19 +5,19 @@ import static org.quackery.Suite.suite;
 
 import org.quackery.Test;
 
-public class TestOptional {
-  public static Test testOptional() {
-    return suite("optional")
+public class TestMaybe {
+  public static Test testMaybe() {
+    return suite("maybe")
         .add(snippetTest("present")
-            .importing("lang.optional.present")
+            .importing("lang.maybe.present")
             .test("present(1)((x){x})(2)", "1"))
         .add(snippetTest("absent")
-            .importing("lang.optional.absent")
+            .importing("lang.maybe.absent")
             .test("absent((x){x})(2)", "2"))
         .add(snippetTest("absent")
-            .importing("lang.optional.else")
-            .importing("lang.optional.present")
-            .importing("lang.optional.absent")
+            .importing("lang.maybe.else")
+            .importing("lang.maybe.present")
+            .importing("lang.maybe.absent")
             .test("else(2)(present(1))", "1")
             .test("else(2)(absent)", "2"));
   }
