b689de46ad7fc161ed77bcc970ad78312b8c5e13
Maciej Mikosik <maciej@mikosik.com>
2023-12-08 17:55:12 +0100

    upgraded logbuddy to version 0.19.1

--- runtime/com/mikosik/stork/debug/Debug.java
+++ runtime/com/mikosik/stork/debug/Debug.java
@@ -1,27 +1,32 @@
 package com.mikosik.stork.debug;
 
-import static org.logbuddy.decorator.ComposedDecorator.compose;
-import static org.logbuddy.decorator.InjectingDecorator.injecting;
 import static org.logbuddy.decorator.InvocationDecorator.invocationDecorator;
-import static org.logbuddy.decorator.TraversingDecorator.traversing;
+import static org.logbuddy.decorator.Rich.traversing;
 import static org.logbuddy.logger.InvocationDepthLogger.invocationDepth;
 import static org.logbuddy.logger.wire.FileLogger.fileLogger;
 
 import java.nio.file.Path;
-import java.util.Map;
 
 import org.logbuddy.Decorator;
 import org.logbuddy.Logger;
 import org.logbuddy.Renderer;
 
+import com.mikosik.stork.compute.Computer;
+
 public class Debug {
   public static Decorator configuredDecorator(Path logFile) {
     Renderer<String> renderer = new StorkTextRenderer();
     Logger logger = invocationDepth(fileLogger(logFile, renderer));
-    Decorator decorator = compose(
-        invocationDecorator(logger),
-        injecting(logger));
-    return traversing(decorator)
-        .filter(field -> !Map.class.isAssignableFrom(field.getDeclaringClass()));
+    return traversing(filteringTypes(invocationDecorator(logger)));
+  }
+
+  private static Decorator filteringTypes(Decorator decorator) {
+    return new Decorator() {
+      public <T> T decorate(T decorable) {
+        return decorable instanceof Computer
+            ? decorator.decorate(decorable)
+            : decorable;
+      }
+    };
   }
 }
