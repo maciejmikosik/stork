dc70c17e443547bdc0672f4ff57af0694191ea52
Maciej Mikosik <maciej@mikosik.com>
2024-02-10 19:43:03 +0100

    enabled program test to assert build problems

--- java/com/mikosik/stork/common/Collections.java
+++ java/com/mikosik/stork/common/Collections.java
@@ -1,11 +1,14 @@
 package com.mikosik.stork.common;
 
 import static com.mikosik.stork.common.Sequence.toSequence;
+import static java.util.Collections.unmodifiableSet;
 import static java.util.Spliterator.ORDERED;
 import static java.util.Spliterators.spliteratorUnknownSize;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.Spliterator;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -31,6 +34,12 @@ public class Collections {
     return element -> clazz.isInstance(element)
         ? Stream.of((B) element)
         : Stream.empty();
+  }
+
+  public static <E> Set<E> intersection(Set<E> a, Set<E> b) {
+    var result = new HashSet<>(a);
+    result.retainAll(b);
+    return unmodifiableSet(result);
   }
 
   public static <E> List<E> flatten(Iterable<? extends Iterable<? extends E>> iterables) {
