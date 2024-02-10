10379e98795b1f2c2c6584cabc1bf4747c7e4f45
Maciej Mikosik <maciej@mikosik.com>
2024-02-10 19:43:03 +0100

    enabled program test to assert build problems

--- java/com/mikosik/stork/test/ProgramTest.java
+++ java/com/mikosik/stork/test/ProgramTest.java
@@ -7,6 +7,7 @@ import static com.mikosik.stork.build.link.CombinatoryModule.combinatoryModule;
 import static com.mikosik.stork.build.link.MathModule.mathModule;
 import static com.mikosik.stork.build.link.problem.VerifyModule.verify;
 import static com.mikosik.stork.common.Check.check;
+import static com.mikosik.stork.common.Collections.intersection;
 import static com.mikosik.stork.common.Sequence.sequence;
 import static com.mikosik.stork.common.io.Ascii.ascii;
 import static com.mikosik.stork.common.io.Buffer.newBuffer;
@@ -18,17 +19,24 @@ import static com.mikosik.stork.program.ProgramModule.programModule;
 import static com.mikosik.stork.test.FsBuilder.fsBuilder;
 import static java.lang.String.format;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toCollection;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import java.util.function.BiFunction;
 
 import org.quackery.Body;
 import org.quackery.Test;
 import org.quackery.report.AssertException;
 
+import com.mikosik.stork.build.BuildException;
+import com.mikosik.stork.build.CannotBuild;
 import com.mikosik.stork.common.io.Buffer;
 import com.mikosik.stork.model.Module;
 import com.mikosik.stork.program.Program;
@@ -44,6 +52,7 @@ public class ProgramTest implements Test {
   private final FsBuilder fsBuilder;
   private byte[] stdin = new byte[0];
   private byte[] expectedStdout = null;
+  private final List<CannotBuild> expectedProblems = new LinkedList<>();
 
   private ProgramTest(String name, Path directory) {
     this.name = name;
@@ -75,7 +84,14 @@ public class ProgramTest implements Test {
   }
 
   public ProgramTest stdout(String expectedStdout) {
+    check(expectedProblems.isEmpty());
     this.expectedStdout = bytes(expectedStdout);
+    return this;
+  }
+
+  public ProgramTest expect(CannotBuild problem) {
+    check(expectedStdout == null);
+    expectedProblems.add(problem);
     return this;
   }
 
@@ -93,11 +109,19 @@ public class ProgramTest implements Test {
   }
 
   private void run() {
-    check(expectedStdout != null);
+    if (expectedStdout != null) {
+      runAndAssertStdout();
+    } else if (!expectedProblems.isEmpty()) {
+      buildAndAssertProblems();
+    } else {
+      throw new RuntimeException();
+    }
+  }
 
-    Module module = join(sequence(
-        build(moduleFromDirectory(fsBuilder.directory)),
-        NATIVE_MODULE));
+  private void runAndAssertStdout() {
+    Module module = build(verify(join(sequence(
+        moduleFromDirectory(fsBuilder.directory),
+        NATIVE_MODULE))));
     Program program = program(identifier("main"), module);
     Buffer buffer = newBuffer();
     program.run(input(stdin), buffer.asOutput());
@@ -110,6 +134,53 @@ public class ProgramTest implements Test {
           + "  %s\n",
           ascii(expectedStdout),
           ascii(actualStdout)));
+    }
+  }
+
+  private void buildAndAssertProblems() {
+    var actual = messagesFrom(buildAndReturnProblems());
+    var expected = messagesFrom(expectedProblems);
+    var common = intersection(actual, expected);
+    actual.removeAll(common);
+    expected.removeAll(common);
+    if (!expected.isEmpty() || !actual.isEmpty()) {
+      throw new AssertException(formatMessage(actual, expected));
+    }
+  }
+
+  private List<? extends CannotBuild> buildAndReturnProblems() {
+    try {
+      verify(join(sequence(
+          moduleFromDirectory(fsBuilder.directory),
+          NATIVE_MODULE)));
+      return emptyList();
+    } catch (BuildException exception) {
+      return exception.problems;
+    }
+  }
+
+  private static Set<String> messagesFrom(List<? extends CannotBuild> problems) {
+    return problems.stream()
+        .map(Object::toString)
+        .collect(toCollection(HashSet::new));
+  }
+
+  private static String formatMessage(Set<String> actual, Set<String> expected) {
+    var builder = new StringBuilder();
+    append(builder, "expected", expected);
+    append(builder, "not expected", actual);
+    return builder.toString();
+  }
+
+  private static void append(
+      StringBuilder builder,
+      String headline,
+      Set<String> problems) {
+    if (!problems.isEmpty()) {
+      builder.append(headline).append("\n\n");
+      for (String problem : problems) {
+        builder.append(problem).append("\n");
+      }
     }
   }
 
