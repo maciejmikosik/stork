e1c6fea5a31ec4db22b82011d34816a796c53cbd
Maciej Mikosik <maciej@mikosik.com>
2024-02-12 14:13:47 +0100

    centralized building of core library

--- java/com/mikosik/stork/Stork.java
+++ java/com/mikosik/stork/Stork.java
@@ -1,47 +1,41 @@
 package com.mikosik.stork;
 
 import static com.mikosik.stork.build.Stars.build;
+import static com.mikosik.stork.build.Stars.buildCoreLibrary;
 import static com.mikosik.stork.build.Stars.moduleFromDirectory;
 import static com.mikosik.stork.build.link.Bind.join;
-import static com.mikosik.stork.build.link.CombinatoryModule.combinatoryModule;
-import static com.mikosik.stork.build.link.MathModule.mathModule;
 import static com.mikosik.stork.build.link.problem.VerifyModule.verify;
 import static com.mikosik.stork.common.Sequence.sequence;
 import static com.mikosik.stork.common.io.Input.input;
 import static com.mikosik.stork.common.io.Output.output;
 import static com.mikosik.stork.model.Identifier.identifier;
 import static com.mikosik.stork.program.Program.program;
-import static com.mikosik.stork.program.ProgramModule.programModule;
 import static java.nio.file.FileSystems.newFileSystem;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
-
-import com.mikosik.stork.model.Module;
 
 public class Stork {
   public static void main(String[] args) {
     var module = build(verify(join(sequence(
-        coreLibraryInsideJar(),
-        moduleFromDirectory(Paths.get(".")),
-        programModule(),
-        combinatoryModule(),
-        mathModule()))));
+        buildCoreLibrary(pathToCoreLibraryInsideJar()),
+        moduleFromDirectory(Paths.get("."))))));
 
     program(identifier("main"), module)
         .run(input(System.in), output(System.out));
   }
 
-  private static Module coreLibraryInsideJar() {
+  private static Path pathToCoreLibraryInsideJar() {
     try {
       // TODO why jar fs needs "core.star"?
       var jarFileSystem = newFileSystem(
           Stork.class.getClassLoader().getResource("core.star").toURI(),
           Map.of("create", "false"));
       var coreStarFileSystem = newFileSystem(jarFileSystem.getPath("/core.star"));
-      return moduleFromDirectory(coreStarFileSystem.getPath("."));
+      return coreStarFileSystem.getPath(".");
     } catch (URISyntaxException | IOException e) {
       throw new LinkageError("", e);
     }
