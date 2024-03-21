02c835125f0d27de3ae6c96e53e59056df4f9bbb
Maciej Mikosik <maciej@mikosik.com>
2024-03-21 22:15:31 +0100

    organized helpers methods that should be in quackery

---
+++ java/com/mikosik/stork/test/QuackeryHelper.java
@@ -0,0 +1,68 @@
+package com.mikosik.stork.test;
+
+import static org.quackery.Suite.suite;
+import static org.quackery.help.Helpers.thrownBy;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import org.quackery.Body;
+import org.quackery.Test;
+
+// TODO implement those functions in quackery
+public class QuackeryHelper {
+  public static boolean isCase(Test test) {
+    return test.visit(
+        (name, body) -> true,
+        (name, children) -> false);
+  }
+
+  public static boolean isSuite(Test test) {
+    return !isCase(test);
+  }
+
+  public static Function<Test, Test> ifCase(BiFunction<String, Body, Test> change) {
+    return test -> test.visit(
+        change::apply,
+        (name, children) -> test);
+  }
+
+  public static Function<Test, Test> ifSuite(BiFunction<String, List<Test>, Test> change) {
+    return test -> test.visit(
+        (name, body) -> test,
+        change::apply);
+  }
+
+  public static Function<Test, Test> filter(Predicate<Test> predicate) {
+    return ifSuite((name, children) -> suite(name)
+        .addAll(children.stream()
+            .filter(predicate)
+            .toList()));
+  }
+
+  public static Function<Test, Test> deep(Function<Test, Test> change) {
+    return test -> test.visit(
+        (name, body) -> change.apply(test),
+        (name, children) -> change.apply(suite(name)
+            .addAll(children.stream()
+                .map(deep(change))
+                .toList())));
+  }
+
+  public static Test filterFailed(Test test) {
+    return deep(filter(t -> t.visit(
+        (name, body) -> thrownBy(body).isPresent(),
+        (name, children) -> !children.isEmpty())))
+            .apply(test);
+  }
+
+  public static int count(Test test) {
+    return test.visit(
+        (name, body) -> 1,
+        (name, children) -> children.stream()
+            .mapToInt(QuackeryHelper::count)
+            .sum());
+  }
+}
