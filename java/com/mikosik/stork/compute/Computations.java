fe813db172928a325d5207ee94b25e34da929b6a
Maciej Mikosik <maciej@mikosik.com>
2025-01-04 20:18:23 +0100

    moved stack depth cache to computations helpers

--- java/com/mikosik/stork/compute/Computations.java
+++ java/com/mikosik/stork/compute/Computations.java
@@ -4,7 +4,11 @@ import static com.mikosik.stork.common.Throwables.runtimeException;
 import static com.mikosik.stork.compute.Computation.computation;
 import static com.mikosik.stork.model.Application.application;
 
+import java.util.Map;
+import java.util.WeakHashMap;
+
 import com.mikosik.stork.compute.Stack.Argument;
+import com.mikosik.stork.compute.Stack.Empty;
 import com.mikosik.stork.compute.Stack.Frame;
 import com.mikosik.stork.compute.Stack.Function;
 import com.mikosik.stork.model.Expression;
@@ -29,4 +33,17 @@ public class Computations {
     }
     return computation.expression;
   }
+
+  public static int depthOf(Stack stack) {
+    return stackDepth.computeIfAbsent(stack, Computations::computeDepthOf);
+  }
+
+  private static int computeDepthOf(Stack stack) {
+    return switch (stack) {
+      case Empty empty -> 0;
+      case Frame frame -> depthOf(frame.previous) + 1;
+    };
+  }
+
+  private static final Map<Stack, Integer> stackDepth = new WeakHashMap<>();
 }
--- java/com/mikosik/stork/debug/StorkTextRenderer.java
+++ java/com/mikosik/stork/debug/StorkTextRenderer.java
@@ -4,27 +4,23 @@ import static com.mikosik.stork.common.io.Serializables.ascii;
 import static com.mikosik.stork.compile.link.Bind.removeNamespaces;
 import static com.mikosik.stork.compute.Computation.computation;
 import static com.mikosik.stork.compute.Computations.abort;
+import static com.mikosik.stork.compute.Computations.depthOf;
 import static com.mikosik.stork.debug.Decompiler.decompile;
 import static com.mikosik.stork.model.Application.application;
 import static com.mikosik.stork.model.Identifier.identifier;
 import static java.lang.String.format;
 
 import java.util.Map;
-import java.util.WeakHashMap;
 
 import org.logbuddy.renderer.TextRenderer;
 
 import com.mikosik.stork.compute.Computation;
 import com.mikosik.stork.compute.Computer;
 import com.mikosik.stork.compute.Stack;
-import com.mikosik.stork.compute.Stack.Empty;
-import com.mikosik.stork.compute.Stack.Frame;
 import com.mikosik.stork.model.Expression;
 
 // TODO Decompiler is tested but renderer is not.
 public final class StorkTextRenderer extends TextRenderer {
-  private final Map<Stack, Integer> stackDepth = new WeakHashMap<>();
-
   protected StorkTextRenderer() {}
 
   public String render(Object model) {
@@ -35,18 +31,6 @@ public final class StorkTextRenderer extends TextRenderer {
       case Stack stack -> format("stack(%s)", depthOf(stack));
       case Map<?, ?> map -> "Map";
       default -> super.render(model);
-    };
-  }
-
-  // TODO separate class for caching stack depth
-  private int depthOf(Stack stack) {
-    return stackDepth.computeIfAbsent(stack, this::computeDepthOf);
-  }
-
-  private int computeDepthOf(Stack stack) {
-    return switch (stack) {
-      case Empty empty -> 0;
-      case Frame frame -> depthOf(frame.previous) + 1;
     };
   }
 
