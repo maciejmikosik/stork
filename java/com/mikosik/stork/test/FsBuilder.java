2dbe9186af15ca240b511a12f8ae88d26ef6a20a
Maciej Mikosik <maciej@mikosik.com>
2025-01-25 15:41:56 +0100

    changed program tests to create files lazily

--- java/com/mikosik/stork/test/FsBuilder.java
+++ java/com/mikosik/stork/test/FsBuilder.java
@@ -1,23 +1,40 @@
 package com.mikosik.stork.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.mikosik.stork.common.io.Directory;
 
 // TODO implement some kind of in-memory filesystem
 public class FsBuilder {
-  public final Directory directory;
+  private Directory directory;
+  private final Map<String, byte[]> files = new HashMap<>();
 
-  private FsBuilder(Directory directory) {
-    this.directory = directory;
+  private FsBuilder() {}
+
+  public static FsBuilder fsBuilder() {
+    return new FsBuilder();
   }
 
-  public static FsBuilder fsBuilder(Directory directory) {
-    return new FsBuilder(directory);
+  public FsBuilder directory(Directory directory) {
+    this.directory = directory;
+    return this;
   }
 
   public FsBuilder file(String path, byte[] content) {
-    directory.file(path)
-        .createDeep()
-        .append(content);
+    files.put(path, content);
+    return this;
+  }
+
+  public FsBuilder create() {
+    files.entrySet().stream()
+        .forEach(entry -> {
+          var path = entry.getKey();
+          var content = entry.getValue();
+          directory.file(path)
+              .createDeep()
+              .append(content);
+        });
     return this;
   }
 
