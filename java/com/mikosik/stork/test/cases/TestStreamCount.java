850212fbfeb73b301dbec7266f8c0bc8820dcaff
Maciej Mikosik <maciej@mikosik.com>
2025-02-09 09:21:08 +0100

    reorganized core functions for counting

---
+++ java/com/mikosik/stork/test/cases/TestStreamCount.java
@@ -0,0 +1,40 @@
+package com.mikosik.stork.test.cases;
+
+import static com.mikosik.stork.test.SnippetSuite.snippetSuite;
+import static org.quackery.Suite.suite;
+
+import org.quackery.Test;
+
+public class TestStreamCount {
+  public static Test testStreamCount() {
+    return suite("stream.count")
+        .add(snippetSuite("count")
+            .importing("lang.stream.count.count")
+            .importing("lang.stream.limit")
+            .importing("lang.stream.each")
+            .importing("lang.integer.add")
+            .test("each(add(97))(limit(5)(count))", "abcde"))
+        .add(snippetSuite("countFrom")
+            .importing("lang.stream.count.countFrom")
+            .importing("lang.stream.limit")
+            .importing("lang.stream.each")
+            .importing("lang.integer.add")
+            .test("limit(5)(countFrom(97))", "abcde")
+            .test("each(add(97))(limit(5)(countFrom(0)))", "abcde")
+            .test("each(add(98))(limit(5)(countFrom(-1)))", "abcde"))
+        .add(snippetSuite("countTo")
+            .importing("lang.stream.count.countTo")
+            .importing("lang.stream.each")
+            .importing("lang.integer.add")
+            .test("each(add(97))(countTo(5))", "abcde")
+            .test("each(add(97))(countTo(0))", "")
+            .test("each(add(97))(countTo(-1))", ""))
+        .add(snippetSuite("countDownFrom")
+            .importing("lang.stream.count.countDownFrom")
+            .importing("lang.stream.each")
+            .importing("lang.integer.add")
+            .test("each(add(97))(countDownFrom(5))", "edcba")
+            .test("each(add(97))(countDownFrom(0))", "")
+            .test("each(add(97))(countDownFrom(-1))", ""));
+  }
+}
