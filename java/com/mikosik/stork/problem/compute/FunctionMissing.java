1c08f48d0c85b7adbff96e3662bc8afff4d9accc
Maciej Mikosik <maciej@mikosik.com>
2025-12-16 11:16:17 +0100

    implemented missing description of computation problem

--- java/com/mikosik/stork/problem/compute/FunctionMissing.java
+++ java/com/mikosik/stork/problem/compute/FunctionMissing.java
@@ -1,6 +1,9 @@
 package com.mikosik.stork.problem.compute;
 
+import static com.mikosik.stork.problem.Description.description;
+
 import com.mikosik.stork.model.Identifier;
+import com.mikosik.stork.problem.Description;
 
 public class FunctionMissing extends CannotCompute {
   private final Identifier function;
@@ -13,8 +16,8 @@ public class FunctionMissing extends CannotCompute {
     return new FunctionMissing(function);
   }
 
-  public String getMessage() {
-    return "function [%s] is missing"
-        .formatted(function.name());
+  public Description describe() {
+    return description("function [%s] is missing"
+        .formatted(function.name()));
   }
 }
