80117fbcba725d79630a2189de388ac5eb0cee98
Maciej Mikosik <maciej@mikosik.com>
2026-05-27 16:03:44 +0200

    rewritten source reader in recursive way

--- java/com/mikosik/stork/compile/SourceReader.java
+++ java/com/mikosik/stork/compile/SourceReader.java
@@ -1,16 +1,12 @@
 package com.mikosik.stork.compile;
 
-import static com.mikosik.stork.common.Collections.stream;
-import static com.mikosik.stork.common.io.Input.input;
-import static com.mikosik.stork.common.io.InputOutput.walk;
-import static com.mikosik.stork.model.Namespace.namespace;
+import static com.mikosik.stork.common.ImmutableList.join;
+import static com.mikosik.stork.common.ImmutableList.list;
+import static com.mikosik.stork.model.Namespace.namespaceOf;
 import static com.mikosik.stork.model.StorkFile.ImportFile.importFile;
 import static com.mikosik.stork.model.StorkFile.SourceFile.sourceFile;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
-import java.util.Optional;
 
 import com.mikosik.stork.common.io.Directory;
 import com.mikosik.stork.model.Namespace;
@@ -18,6 +14,7 @@ import com.mikosik.stork.model.StorkFile;
 import com.mikosik.stork.model.StorkFile.ImportFile;
 import com.mikosik.stork.model.StorkFile.SourceFile;
 
+// TODO rename to something better
 public class SourceReader {
   private SourceReader() {}
 
@@ -26,35 +23,37 @@ public class SourceReader {
   }
 
   public List<StorkFile> read(Directory directory) {
-    // TODO implement custom walking algorithm
-    // TODO skip inaccessible nodes
-    // TODO skip names with dots, like hidden directories/files
-    return walk(directory.path)
-        .filter(Files::isRegularFile)
-        .map(file -> storkFile(
-            file.getFileName().toString(),
-            namespaceFor(directory.path, file.getParent()),
-            input(file).readAllBytes()))
-        .flatMap(Optional::stream)
-        .toList();
+    return readDeep(directory, namespaceOf());
   }
 
-  private Optional<StorkFile> storkFile(
-      String fileName,
-      Namespace namespace,
-      byte[] content) {
-    return switch (fileName) {
-      case SourceFile.FILE_NAME -> Optional.of(sourceFile(namespace, content));
-      case ImportFile.FILE_NAME -> Optional.of(importFile(namespace, content));
-      default -> Optional.empty();
-    };
+  private static List<StorkFile> readDeep(
+      Directory directory,
+      Namespace namespace) {
+    return join(
+        readCurrent(directory, namespace),
+        directory.directories()
+            // TODO skip names with illegal characters
+            // TODO skip inaccessible directories
+            .map(subdirectory -> readDeep(
+                subdirectory,
+                namespace.add(subdirectory.name())))
+            .flatMap(List::stream)
+            .toList());
   }
 
-  private static Namespace namespaceFor(Path directory, Path subdirectory) {
-    var components = stream(subdirectory.iterator())
-        .skip(directory.getNameCount())
-        .map(path -> path.toString())
-        .toList();
-    return namespace(components);
+  private static List<StorkFile> readCurrent(
+      Directory directory,
+      Namespace namespace) {
+    return list(
+        importFile(
+            namespace,
+            directory.file(ImportFile.FILE_NAME)
+                .tryInput()
+                .readAllBytes()),
+        sourceFile(
+            namespace,
+            directory.file(SourceFile.FILE_NAME)
+                .tryInput()
+                .readAllBytes()));
   }
 }
