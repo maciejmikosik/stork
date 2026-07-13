57148f01815540ce9c2d8494d6d0cde30f363033
Maciej Mikosik <maciej@mikosik.com>
2026-07-13 08:37:21 +0200

    used new functional features in compiler

--- java/com/mikosik/stork/compile/Compiler.java
+++ java/com/mikosik/stork/compile/Compiler.java
@@ -1,14 +1,12 @@
 package com.mikosik.stork.compile;
 
-import static com.mikosik.stork.common.Collections.iterator;
+import static com.mikosik.stork.common.Collections.each;
 import static com.mikosik.stork.common.ImmutableList.join;
-import static com.mikosik.stork.common.func.Functions.faa;
+import static com.mikosik.stork.common.func.On.on;
 import static com.mikosik.stork.compile.Importer.importer;
 import static com.mikosik.stork.compile.link.Bridge.stork;
 import static com.mikosik.stork.compile.link.Unlambda.unlambda;
 import static com.mikosik.stork.compile.link.VerifyLibrary.findLinkingProblems;
-import static com.mikosik.stork.compile.parse.Parser.parse;
-import static com.mikosik.stork.compile.tokenize.Tokenizer.tokenize;
 import static com.mikosik.stork.model.Identifier.identifier;
 import static com.mikosik.stork.model.change.Changes.deep;
 import static com.mikosik.stork.model.change.Changes.ifLambda;
@@ -16,14 +14,15 @@ import static com.mikosik.stork.model.change.Changes.ifQuote;
 import static com.mikosik.stork.model.change.Changes.ifVariable;
 import static com.mikosik.stork.model.change.Changes.onBody;
 import static com.mikosik.stork.model.change.Changes.onIdentifier;
-import static com.mikosik.stork.model.change.Changes.onNamespace;
-import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.toMap;
 
 import java.util.List;
 
+import com.mikosik.stork.common.Collections;
 import com.mikosik.stork.common.func.Functions.Faa;
+import com.mikosik.stork.compile.parse.Parser;
+import com.mikosik.stork.compile.tokenize.Tokenizer;
 import com.mikosik.stork.model.Definition;
-import com.mikosik.stork.model.Expression;
 import com.mikosik.stork.model.Namespace;
 import com.mikosik.stork.model.StorkDirectory;
 
@@ -45,51 +44,45 @@ public class Compiler {
   }
 
   private static List<Definition> compile(List<StorkDirectory> directories) {
+    // TODO aggregate compiler problems from stream
     var compiled = directories.stream()
-        // TODO aggregate compiler problems from stream
-        .map(directory -> makeComputable(directory.namespace)
-            .apply(compile(directory.sourceFile.content)))
+        .map(directory -> on(directory.sourceFile.content)
+            .map(Collections::iterator)
+            .map(Tokenizer::tokenize)
+            .map(Parser::parse)
+            .map(each(onBody(deep(ifLambda(lambda -> on(lambda)
+                .map(deep(ifVariable(variable -> variable.name.equals(lambda.parameter.name)
+                    ? lambda.parameter
+                    : variable)))
+                .get())))))
+            .map(bind(directory.namespace))
+            .get())
         .flatMap(List::stream)
-        .toList();
-
-    var importer = importer(directories);
-    var linked = compiled.stream()
-        .map(importer::injectInto)
-        .toList();
-    return linked;
-  }
-
-  private static Faa<List<Definition>> makeComputable(
-      Namespace namespace) {
-    return definitions -> definitions.stream()
-        // TODO test that order is ensured: lambda, local, import
-        .map(onBody(bindLambdaParameters))
-        .map(onBody(bindLocalFunctions(namespace, definitions)))
-        .map(onIdentifier(onNamespace(faa(x -> namespace))))
         .map(onBody(unlambda))
         .map(onBody(deep(ifQuote(quote -> stork(quote.string)))))
         .toList();
+
+    var importer = importer(directories);
+    return compiled.stream()
+        .map(importer::injectInto)
+        .toList();
   }
 
-  private static final Faa<Expression> bindLambdaParameters = deep(
-      ifLambda(lambda -> deep(ifVariable(variable -> {
-        return variable.name.equals(lambda.parameter.name)
-            ? lambda.parameter
-            : variable;
-      })).apply(lambda)));
+  private static Faa<List<Definition>> bind(Namespace namespace) {
+    return definitions -> {
+      var table = definitions.stream()
+          .map(definition -> definition.identifier.variable)
+          .collect(toMap(
+              variable -> variable,
+              variable -> identifier(namespace, variable),
+              (value, duplicate) -> value));
 
-  private static Faa<Expression> bindLocalFunctions(
-      Namespace namespace,
-      List<Definition> definitions) {
-    var localFunctions = definitions.stream()
-        .map(definition -> definition.identifier.variable)
-        .collect(toSet());
-    return deep(ifVariable(variable -> localFunctions.contains(variable)
-        ? identifier(namespace, variable)
-        : variable));
-  }
-
-  private static List<Definition> compile(byte[] content) {
-    return parse(tokenize(iterator(content)));
+      return definitions.stream()
+          .map(onIdentifier(identifier -> table.get(identifier.variable)))
+          .map(onBody(deep(ifVariable(variable -> table.containsKey(variable)
+              ? table.get(variable)
+              : variable))))
+          .toList();
+    };
   }
 }
