dce6c992158fba27d906aa75717f50ec37d9e3ee
Maciej Mikosik <maciej@mikosik.com>
2026-07-28 15:06:50 +0200

    made outcomes self-described

--- java/com/mikosik/stork/test/Assertions.java
+++ java/com/mikosik/stork/test/Assertions.java
@@ -1,9 +1,6 @@
 package com.mikosik.stork.test;
 
-import static com.mikosik.stork.common.Throwables.runtimeException;
 import static com.mikosik.stork.common.text.Outline.outline;
-import static com.mikosik.stork.problem.Describer.describe;
-import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Objects.deepEquals;
 
 import java.util.Objects;
@@ -11,8 +8,6 @@ import java.util.Objects;
 import org.quackery.report.AssertException;
 
 import com.mikosik.stork.common.text.Outline;
-import com.mikosik.stork.problem.compile.CannotCompile;
-import com.mikosik.stork.problem.compute.CannotCompute;
 
 public class Assertions {
   public static void assertMatch(Outline expected, Outline actual) {
@@ -34,31 +29,9 @@ public class Assertions {
 
   private static Outline failureMessage(Outcome expected, Outcome actual) {
     return outline("test failed because")
-        .nest(outline("expected " + nameOf(expected))
-            .nest(describeOutcome(expected)))
-        .nest(outline("found " + nameOf(actual))
-            .nest(describeOutcome(actual)));
-  }
-
-  private static String nameOf(Outcome outcome) {
-    return switch (outcome.object) {
-      case byte[] b -> "stdout";
-      case CannotCompile cannotCompile -> "problem";
-      case CannotCompute cannotCompute -> "problem";
-      default -> throw runtimeException("" + outcome.object);
-    };
-  }
-
-  private static Outline describeOutcome(Outcome outcome) {
-    return switch (outcome.object) {
-      case byte[] stdout -> outline(format(stdout));
-      case CannotCompile cannotCompile -> describe(cannotCompile);
-      case CannotCompute cannotCompute -> describe(cannotCompute);
-      default -> throw runtimeException("" + outcome.object);
-    };
-  }
-
-  private static String format(byte[] bytes) {
-    return "[" + new String(bytes, UTF_8) + "]";
+        .nest(outline("expected")
+            .nest(expected.describe()))
+        .nest(outline("found")
+            .nest(actual.describe()));
   }
 }
--- java/com/mikosik/stork/test/Outcome.java
+++ java/com/mikosik/stork/test/Outcome.java
@@ -1,11 +1,17 @@
 package com.mikosik.stork.test;
 
+import static com.mikosik.stork.common.Throwables.runtimeException;
+import static com.mikosik.stork.common.text.Outline.outline;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import com.mikosik.stork.common.Model;
+import com.mikosik.stork.common.text.Outline;
+import com.mikosik.stork.problem.Describer;
 import com.mikosik.stork.problem.compile.CannotCompile;
 import com.mikosik.stork.problem.compute.CannotCompute;
 
 public class Outcome extends Model {
-  public final Object object;
+  private final Object object;
 
   private Outcome(Object object) {
     this.object = object;
@@ -21,5 +27,18 @@ public class Outcome extends Model {
 
   public static Outcome outcome(byte[] stdout) {
     return new Outcome(stdout);
+  }
+
+  public Outline describe() {
+    return switch (object) {
+      case byte[] stdout -> outline(format(stdout));
+      case CannotCompile cannotCompile -> Describer.describe(cannotCompile);
+      case CannotCompute cannotCompute -> Describer.describe(cannotCompute);
+      default -> throw runtimeException("unknown object " + object);
+    };
+  }
+
+  private static String format(byte[] bytes) {
+    return "[" + new String(bytes, UTF_8) + "]";
   }
 }
