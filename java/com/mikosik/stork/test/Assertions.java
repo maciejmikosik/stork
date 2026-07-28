947266c7d8bef7c3e0db3cf457536ac730ee4ade
Maciej Mikosik <maciej@mikosik.com>
2026-07-28 15:08:32 +0200

    cleaned up assertions code

--- java/com/mikosik/stork/test/Assertions.java
+++ java/com/mikosik/stork/test/Assertions.java
@@ -1,21 +1,18 @@
 package com.mikosik.stork.test;
 
 import static com.mikosik.stork.common.text.Outline.outline;
+import static com.mikosik.stork.test.QuackeryHelper.assertException;
 import static java.util.Objects.deepEquals;
-
-import java.util.Objects;
-
-import org.quackery.report.AssertException;
 
 import com.mikosik.stork.common.text.Outline;
 
 public class Assertions {
   public static void assertMatch(Outline expected, Outline actual) {
-    if (!Objects.equals(expected, actual)) {
-      throw new AssertException(outline("test failed because")
+    if (!deepEquals(expected, actual)) {
+      throw assertException(outline("test failed because")
           .nest(outline("expected")
               .nest(expected))
-          .nest(outline("found")
+          .nest(outline("actual")
               .nest(actual))
           .toString());
     }
@@ -23,15 +20,12 @@ public class Assertions {
 
   public static void assertMatch(Outcome expected, Outcome actual) {
     if (!deepEquals(expected, actual)) {
-      throw new AssertException(failureMessage(expected, actual).toString());
+      throw assertException(outline("test failed because")
+          .nest(outline("expected")
+              .nest(expected.describe()))
+          .nest(outline("actual")
+              .nest(actual.describe()))
+          .toString());
     }
-  }
-
-  private static Outline failureMessage(Outcome expected, Outcome actual) {
-    return outline("test failed because")
-        .nest(outline("expected")
-            .nest(expected.describe()))
-        .nest(outline("found")
-            .nest(actual.describe()));
   }
 }
