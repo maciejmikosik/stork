589bd9a160050576ba94ba99850a1b6d869a2232
Maciej Mikosik <maciej@mikosik.com>
2026-09-24 16:22:12 +0200

    checked if source.stork exists in working directory

--- java/com/mikosik/stork/Stork.java
+++ java/com/mikosik/stork/Stork.java
@@ -7,6 +7,7 @@ import static com.mikosik.stork.common.io.Input.input;
 import static com.mikosik.stork.common.io.Output.output;
 import static com.mikosik.stork.compile.Codebase.codebase;
 import static com.mikosik.stork.compile.Compiler.compile;
+import static com.mikosik.stork.compile.SourceReader.SOURCE_FILENAME;
 import static com.mikosik.stork.compile.SourceReader.sourceReader;
 import static com.mikosik.stork.compile.err.Describer.describe;
 import static com.mikosik.stork.model.exp.Identifier.identifier;
@@ -25,14 +26,7 @@ import com.mikosik.stork.compute.err.ComputerException;
 public class Stork {
   public static void main(String[] args) {
     try {
-      var library = compile(codebase()
-          .directories(sourceReader().read(workingDirectory()))
-          .dependencies(core(PRODUCTION))
-          .build());
-      runner().run(task(
-          program(identifier(variable("main")), library),
-          terminal(input(System.in), output(FileDescriptor.out))));
-      System.exit(0);
+      runMain();
     } catch (CompilerException compilerException) {
       System.err.println(describe(compilerException));
       System.exit(1);
@@ -48,6 +42,24 @@ public class Stork {
         throw e;
       }
     }
+  }
+
+  private static void runMain() {
+    var workingDirectory = workingDirectory();
+    var mainSourceFile = workingDirectory.file(SOURCE_FILENAME);
+    if (!mainSourceFile.exists()) {
+      System.err.println("file %s does not exist"
+          .formatted(mainSourceFile));
+      System.exit(1);
+    }
+    var library = compile(codebase()
+        .directories(sourceReader().read(workingDirectory))
+        .dependencies(core(PRODUCTION))
+        .build());
+    runner().run(task(
+        program(identifier(variable("main")), library),
+        terminal(input(System.in), output(FileDescriptor.out))));
+    System.exit(0);
   }
 
   private static boolean isMessage(
